e193a0b165edecefd1154fdc8836da0b22f92858
Edward Faulkner <ef@alum.mit.edu>
2017-05-12 13:08:47 -0400

    progress on imagemagick-bindings based resizer

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,43 @@
-#![feature(time2)]
-use std::path::Path;
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+extern crate rocket;
+use rocket::response::{Response,Content};
+use rocket::http::ContentType;
+
+extern crate magick_rust;
+use magick_rust::{MagickWand, magick_wand_genesis};
+use std::sync::{Once, ONCE_INIT};
 use std::io;
-use std::error::Error;
+use std::vec::Vec;
 
-extern crate image;
+// Used to make sure MagickWand is initialized exactly once. Note that we
+// do not bother shutting down, we simply exit when we're done.
+static START: Once = ONCE_INIT;
 
-extern crate iron;
-use iron::prelude::*;
-use iron::mime::Mime;
-use iron::status;
-use iron::response::WriteBody;
+fn _resize() -> Result<Vec<u8>, &'static str> {
+    START.call_once(|| {
+        magick_wand_genesis();
+    });
+    let wand = MagickWand::new();
+    wand.read_image("test.jpg")?;
+    wand.fit(240, 240);
+    wand.write_image_blob("JPG")
+}
 
-extern crate urlencoded;
-use urlencoded::UrlEncodedQuery;
-
-use std::time::SystemTime;
-
-#[derive(Clone)]
-struct MyImage(image::DynamicImage);
-
-impl WriteBody for MyImage {
-    fn write_body(&mut self, res: &mut iron::response::ResponseBody) -> io::Result<()> {
-        let &mut MyImage(ref i) = self;
-        match i.save(res, image::ImageFormat::JPEG) {
-            Ok(value) => Ok(value),
-            Err(image::ImageError::IoError(err)) => Err(err),
-            Err(image_err) => Err(io::Error::new(io::ErrorKind::Other, image_err.description()))
-        }
+// magick_rust returns string errors, which seems dubious and requires us to convert them.
+fn resize() -> io::Result<Vec<u8>> {
+    match _resize() {
+        Ok(bytes) => Ok(bytes),
+        Err(err_string) => Err(io::Error::new(io::ErrorKind::Other, err_string))
     }
 }
 
-fn jpeg_type() -> Mime {
-    "image/jpeg".parse::<Mime>().unwrap()
-}
-
-fn make_thumbnail(original: image::DynamicImage, size: u32) -> Box<MyImage> {
-    Box::new(MyImage(original.resize(size, size, image::imageops::FilterType::Lanczos3)))
-}
-
-fn log_time<T, R>(label: &str, operation: T) -> R
-    where T : Fn() -> R {
-        let start = SystemTime::now();
-        let output = operation();
-        let duration = start.elapsed().unwrap();
-        println!("{} {}ms", label, duration.as_secs() * 1000 + (duration.subsec_nanos() / 1000000) as u64);
-        output
-}
-
-
-fn get_integer_param(req: &mut Request, key : &str, default_value : u32) -> u32 {
-    req.get_ref::<UrlEncodedQuery>().ok().and_then(|hashmap| {
-        hashmap.get(key)
-    }).and_then(|values| {
-        values.get(0)
-    }).and_then(|string_value| {
-        string_value.parse::<u32>().ok()
-    }).unwrap_or(default_value)
-}
-
-
 fn main() {
-    let content_type = jpeg_type();
-    let original = log_time("load original", || image::open(&Path::new("test.jpg")).unwrap());
-    let closure = move |req: &mut Request| {
-        let size = get_integer_param(req, "size", 600);
-        let thumbnail = log_time("make_thumbnail", || make_thumbnail(log_time("original.clone", ||original.clone()), size));
+    rocket::ignite().mount("/", routes![index]).launch();
+}
 
-        // This cast is needed because Modifier<Result> is implemented
-        // for the trait object type Box<WriteBody + Send>.
-        let b : Box<WriteBody + Send> = thumbnail;
-
-        Ok(Response::with((content_type.clone(), status::Ok, b)))
-    };
-    
-    Iron::new(closure).http("localhost:3000").unwrap();
+#[get("/")]
+fn index() -> io::Result<Content<Response<'static>>> {
+    let bytes = resize()?;
+    Ok(Content(ContentType::JPEG, Response::build().sized_body(std::io::Cursor::new(bytes)).finalize()))
 }
