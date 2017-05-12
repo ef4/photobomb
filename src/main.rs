cf8488631692827a0bee4935e76f467acaf3e35a
Edward Faulkner <ef@alum.mit.edu>
2017-05-12 14:34:09 -0400

    working default params

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 extern crate rocket;
 use rocket::response::{Stream,Content};
 use rocket::http::ContentType;
+use rocket::request::{FromFormValue,FromForm};
 
 extern crate magick_rust;
 use magick_rust::{MagickWand, magick_wand_genesis};
@@ -14,18 +15,19 @@ use std::vec::Vec;
 // do not bother shutting down, we simply exit when we're done.
 static START: Once = ONCE_INIT;
 
-fn _resize(filename: &str, fit: usize) -> Result<Vec<u8>, &'static str> {
+fn _resize(filename: &str, fit: FitSize) -> Result<Vec<u8>, &'static str> {
     START.call_once(|| {
         magick_wand_genesis();
     });
     let wand = MagickWand::new();
     wand.read_image(filename)?;
-    wand.fit(fit, fit);
+    let FitSize(ufit) = fit;
+    wand.fit(ufit, ufit);
     wand.write_image_blob("JPG")
 }
 
 // magick_rust returns string errors, which seems dubious and requires us to convert them.
-fn resize(filename: &str, fit: usize) -> io::Result<Vec<u8>> {
+fn resize(filename: &str, fit: FitSize) -> io::Result<Vec<u8>> {
     match _resize(filename, fit) {
         Ok(bytes) => Ok(bytes),
         Err(err_string) => Err(io::Error::new(io::ErrorKind::Other, err_string))
@@ -33,22 +35,44 @@ fn resize(filename: &str, fit: usize) -> io::Result<Vec<u8>> {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index]).mount("/", routes![index_defaults]).launch();
+}
+
+struct FitSize(usize);
+
+impl<'v> FromFormValue<'v> for FitSize {
+    type Error = &'v str;
+    fn from_form_value(form_value: &'v str) -> Result<FitSize, &'v str> {
+        let pixels = usize::from_form_value(form_value)?;
+        Ok(FitSize(pixels))
+    }
+    fn default() -> Option<Self> {
+        Some(FitSize(100))
+    }
 }
 
 #[derive(FromForm)]
 struct ImageOptions {
-    fit: Option<usize>
+    fit: FitSize
 }
 
+fn default_options() -> ImageOptions {
+    let mut items = rocket::request::FormItems::from("");
+    match ImageOptions::from_form_items(&mut items) {
+        Ok(i) => i,
+        Err(e) => panic!(e)
+    }
+}
 
 #[get("/<filename>?<image_options>")]
 fn index(filename: &str, image_options: ImageOptions) -> io::Result<Content<Stream<io::Cursor<Vec<u8>>>>> {
+    let bytes = resize(filename, image_options.fit)?;
+    Ok(Content(ContentType::JPEG, Stream::from(io::Cursor::new(bytes))))
+}
 
-    let fit = match image_options.fit {
-        Some(pixels) => pixels,
-        None => 100
-    };
-    let bytes = resize(filename, fit)?;
+#[get("/<filename>")]
+fn index_defaults(filename: &str) -> io::Result<Content<Stream<io::Cursor<Vec<u8>>>>> {
+    let image_options = default_options();
+    let bytes = resize(filename, image_options.fit)?;
     Ok(Content(ContentType::JPEG, Stream::from(io::Cursor::new(bytes))))
 }
